ad49af2a9d7510736fa163657f7eb4dd1e3d4340
Booster-663 <botboosterso@gmail.com>
2025-03-08 10:29:33 +0500

    Add files via upload

---
+++ Wallet/script.js
@@ -0,0 +1,39 @@
+function openn() {
+    const bg = document.querySelector(".bg")
+    bg.style = "top: 150%; width: 260px; height: 260px;"
+    document.querySelector(".open").style = "opacity: 0;"
+    const wallet = document.querySelector(".wallet")
+    wallet.style = "width: 260px; height: 260px;"
+}
+
+function openCards() {
+    const background = document.querySelector(".card-background")
+    background.querySelector(".cards").style.display = "block"
+    background.style.display = "block"
+    background.style.opacity = 1
+}
+
+function openPhoto() {
+    const background = document.querySelector(".card-background")
+    background.querySelector(".photo").style.display = "block"
+    background.style.display = "block"
+    background.style.opacity = 1
+}
+
+function openNote() {
+    const background = document.querySelector(".card-background")
+    background.querySelector(".note").style.display = "block"
+    background.style.display = "block"
+    background.style.opacity = 1
+}
+
+function back() {
+    const background = document.querySelector(".card-background")
+    background.style.opacity = 0
+    setTimeout(function(){
+        background.style.display = "none"
+        background.querySelector(".cards").style.display = "none"
+        background.querySelector(".photo").style.display = "none"
+        background.querySelector(".note").style.display = "none"
+    }, 400)
+}
